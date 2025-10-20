9442df1a3f5a12e66c056ae90ec6a2aed15b1cc6
maryanchik111 <loonklage17@gmail.com>
2025-10-20 20:28:14 +0300

    gg

--- .next/server/app/api/apply/route.js
+++ .next/server/app/api/apply/route.js
@@ -1,6 +1,6 @@
 var R=require("../../../chunks/[turbopack]_runtime.js")("server/app/api/apply/route.js")
-R.c("server/chunks/node_modules_next_3e838b7d._.js")
-R.c("server/chunks/[root-of-the-server]__a1bc4c23._.js")
-R.m("[project]/.next-internal/server/app/api/apply/route/actions.js [app-rsc] (server actions loader, ecmascript)")
-R.m("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/src/app/api/apply/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)")
-module.exports=R.m("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/src/app/api/apply/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)").exports
+R.c("server/chunks/[root-of-the-server]__430f83c0._.js")
+R.c("server/chunks/[root-of-the-server]__703022d1._.js")
+R.m(59789)
+R.m(61902)
+module.exports=R.m(61902).exports
--- .next/static/chunks/src_app_apply_page_tsx_423dc2ab._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_app_apply_page_tsx_534f4658._.js"
-],
-    source: "dynamic"
-});
